8eaa116e290c74b599dc9cdd3ce6dce98c48826d
Kiru <kirushanthh13@gmail.com>
2025-07-05 09:25:01 +0530

    Json annotations

--- src/main/java/com/codewithmosh/store/dtos/UserDto.java
+++ src/main/java/com/codewithmosh/store/dtos/UserDto.java
@@ -1,12 +1,20 @@
 package com.codewithmosh.store.dtos;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 
 @AllArgsConstructor
 @Getter
 public class UserDto {
+    @JsonIgnore
     private Long id;
+    @JsonProperty("full_name")
     private String name;
     private String email;
+
+    @JsonInclude(JsonInclude.Include.NON_NULL)
+    private String phoneNumber;
 }
