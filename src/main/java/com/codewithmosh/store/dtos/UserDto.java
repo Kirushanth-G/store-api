aa62feb4a1a4b3bd01c1f3a24f15ff3f35cdccdd
Kiru <kirushanthh13@gmail.com>
2025-07-05 09:30:43 +0530

    JsonFormat annotation for datetime

--- src/main/java/com/codewithmosh/store/dtos/UserDto.java
+++ src/main/java/com/codewithmosh/store/dtos/UserDto.java
@@ -1,10 +1,13 @@
 package com.codewithmosh.store.dtos;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
 import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.fasterxml.jackson.annotation.JsonInclude;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
+
+import java.time.LocalDateTime;
 
 @AllArgsConstructor
 @Getter
@@ -17,4 +20,7 @@ public class UserDto {
 
     @JsonInclude(JsonInclude.Include.NON_NULL)
     private String phoneNumber;
+
+    @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss")
+    private LocalDateTime createdAt;
 }
